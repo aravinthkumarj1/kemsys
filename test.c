20b6db9f360a17fc4562d666a31ece2bf15b6f26
Aravinthkumar <aravinthkumarj1@gmail.com>
2016-01-06 23:01:27 +0530

    Array to int

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+char buf[10];
+scanf("%c",&buf);
+printf("buff is %s\n",buf);
+int buffer;
+        buffer = atoi(buf);
+        printf("Buffer is %d\n",buffer);
+return 0;
+}
